67dfc71c84ad83904e1ee1e75f86dc68cb2bc886
Craig M. Stimmel <cstimmel@uw.edu>
2016-02-16 12:28:24 -0800

    Include setup.py.

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='SpaceScout-Labstats',
+      version='1.0',
+      description='Labstats bridge for SpaceScout',
+      install_requires=[
+                        'Django>=1.4,<1.5',
+                        'oauth2<=1.5.211',
+                        'simplejson>=2.1',
+                        'soappy',
+                        'poster',
+                       ],
+     )
