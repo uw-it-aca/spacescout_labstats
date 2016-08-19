0b5fde97d29cb3824936856d85bf2abb1a98cc81
Bradley Bell <bradleyb@uw.edu>
2016-08-19 15:37:19 -0700

    Implement search and validation methods

--- spacescout_labstats/endpoints/cte_techloan.py
+++ spacescout_labstats/endpoints/cte_techloan.py
@@ -1,23 +1,22 @@
 """
-This file contains a template of what an endpoint file should look like.
-
-It also contains more in depth descriptions of the individual methods, what
-they should do, and how.
-
-
+This file contains methods which are used to update the online labstats spaces.
 """
+import logging
+from django.conf import settings
+
+logger = logging.getLogger(__name__)
 
 
 def get_space_search_parameters():
     """
-    This method will return the URL to be queried to search for spaces to be
-    updated. For example, the online_labstats has the following URL:
-
-    {server}/api/v1/spot/?extended_info:has_online_labstats=true&limit=0
-
-    This tells the server to return all spaces with has_online_labstats= true,
-    with no limit.
+    Returns the URL with which to send a GET request to spaceseeker_server and
+    retrieve all the spaces that need to be updated from cte_techloan.
     """
+    url = ("%s/api/v1/spot/?extended_info:app_type=tech&"
+           "extended_info:has_cte_techloan=true&"
+           "limit=0") \
+        % (settings.SS_WEB_SERVER_HOST)
+    return url
 
 
 def get_name():
@@ -25,25 +24,31 @@ def get_name():
     This will return the name of this endpoint, primarily for logging
     purposes.
     """
-    return "Endpoint Template"
+    return "CTE Tech Loan"
 
 
 def validate_space(space):
     """
     This method will validate a space, thus ensuring that it is compliant with
-    the standards requisite for being updated by the endpoint.
+    the standards requisite for being updated by the cte tech loan endpoint.
 
     This method should only be called with spaces already validated by the
     utils.validate_space
     """
+    if "cte_techloan_id" not in space["extended_info"]:
+        raise Exception("cte_techloan_id not present in space #" +
+                        str(space['id']))
 
 
-def get_endpoint_data(spaces_to_be_updated):
+def get_endpoint_data(techloan_spaces):
     """
     This is the method that will be called once the spaces have been retrieved
     by a GET to spaceseeker_server with get_space_search_parameters. You should
     then query your endpoint for the data to update these spaces, update the
     spaces(in their original dictionary).
+
+    will retrieve item availability from the cte tech loan server and update
+    the items in these spaces appropriately
     """
 
 
