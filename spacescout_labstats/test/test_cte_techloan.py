d28c5d2bcb5d6c8e5d20c705e8e0cef514bb7e4a
Craig M. Stimmel <cstimmel@uw.edu>
2018-08-30 15:19:37 -0700

    Add test scaffold for cte_techloan endpoint.

---
+++ spacescout_labstats/test/test_cte_techloan.py
@@ -0,0 +1,11 @@
+"""
+This file contians tests for cte_techloan.py
+"""
+from spacescout_labstats.endpoints import cte_techloan
+from . import LabstatsTestCase
+
+
+class CTETechloanTest(LabstatsTestCase):
+
+    def test_hello(self):
+        self.assertTrue(True)
