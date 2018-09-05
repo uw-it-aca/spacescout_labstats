e812a184e13532c1a8a29eaa348ad565665fc3fd
Keith Roberts <keithrob@uw.edu>
2018-09-05 13:52:57 -0700

    Added tests for get name and get space search parameters

--- spacescout_labstats/test/test_cte_techloan.py
+++ spacescout_labstats/test/test_cte_techloan.py
@@ -1,6 +1,8 @@
 """
 This file contians tests for cte_techloan.py
 """
+from django.conf import settings
+
 from spacescout_labstats.endpoints import cte_techloan
 from . import LabstatsTestCase
 
@@ -9,3 +11,22 @@ class CTETechloanTest(LabstatsTestCase):
 
     def test_hello(self):
         self.assertTrue(True)
+
+    def test_get_space_search_parameters(self):
+        """
+        Tests that the url constructed for GET requests to spotseeker_server
+        is correct and is generated successfully
+        """
+        server_host = settings.SS_WEB_SERVER_HOST
+        expected_url = ("%s/api/v1/spot/?extended_info:app_type=tech&"
+                        "extended_info:has_cte_techloan=true&"
+                        "limit=0") % (server_host)
+        # Assert that the retireved url matches the expected url
+        self.assertEquals(expected_url, cte_techloan.get_space_search_parameters())
+
+    def test_get_name(self):
+        """
+        Tests that the name of the endpoint is correct (logging purposes)
+        """
+        expected_name = "CTE Tech Loan"
+        self.assertEquals(expected_name, cte_techloan.get_name())
